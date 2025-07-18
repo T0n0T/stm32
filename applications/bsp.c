a65dd91c3fc4f625c4a92842e6ed10e99cbd5535
T0n0T <823478402@qq.com>
2025-07-18 14:22:33 +0000

    add trace

--- applications/bsp.c
+++ applications/bsp.c
@@ -72,6 +72,29 @@ void BSP_init(void)
      */
     SystemCoreClockUpdate();
     cm_backtrace_init("STM32L4", "V1.0", "1.0.0");
+    DBGMCU->CR |= DBGMCU_CR_TRACE_IOEN;
+    CoreDebug->DEMCR |= CoreDebug_DEMCR_TRCENA_Msk;
+    TPI->ACPR = 15; 
+    TPI->SPPR = 2;
+    TPI->FFCR = 0x100;
+    DWT->CTRL = (1 << DWT_CTRL_CYCTAP_Pos)       // Prescaler for PC sampling
+                                                 // 0 = x64, 1 = x1024
+                | (0 << DWT_CTRL_POSTPRESET_Pos) // Postscaler for PC sampling
+                                                 // Divider = value + 1
+                | (1 << DWT_CTRL_PCSAMPLENA_Pos) // Enable PC sampling
+                | (2 << DWT_CTRL_SYNCTAP_Pos)    // Sync packet interval
+                                                 // 0 = Off, 1 = Every 2^23 cycles,
+                                                 // 2 = Every 2^25, 3 = Every 2^27
+                | (1 << DWT_CTRL_EXCTRCENA_Pos)  // Enable exception trace
+                | (1 << DWT_CTRL_CYCCNTENA_Pos); // Enable cycle counter
+
+    /* Configure instrumentation trace macroblock */
+    ITM->LAR = 0xC5ACCE55;
+    ITM->TCR = (1 << ITM_TCR_TraceBusID_Pos) // Trace bus ID for TPIU
+               | (1 << ITM_TCR_DWTENA_Pos)   // Enable events from DWT
+               | (1 << ITM_TCR_SYNCENA_Pos)  // Enable sync packets
+               | (1 << ITM_TCR_ITMENA_Pos);  // Main enable for ITM
+    ITM->TER = 0xFFFFFFFF;                   // Enable all stimulus ports
     HAL_Init();
     led_init();   /* initialize the LEDs */
     usart_init(); /* initialize the USART */
